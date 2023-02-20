439f0cb3adad080e1f9832edbbf9f6369d875104
Abdulwadood <mawsakehm60@gmail.com>
2023-02-20 22:44:46 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+let btn = document.getElementById('tryy');
+btn.onclick = function (){
+ st = document.getElementById('menu');
+ let current = st.style.display 
+ if(current == "none"){
+
+ st.style.display="block";
+ }
+ else{
+     st.style.display="none";
+ }
+ 
+}
